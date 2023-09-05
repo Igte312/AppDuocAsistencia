1fe21bc3b08923b410eb78de0df625b5c070b27e
Igte312 <ignacioloptez@gmail.com>
2023-09-04 23:18:00 -0300

    listo los cambios falta animacion

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -15,6 +15,10 @@ const routes: Routes = [
     path: 'login',
     loadChildren: () => import('./login/login.module').then( m => m.LoginPageModule)
   },
+  {
+    path: 'recupera-contrasena',
+    loadChildren: () => import('./recupera-contrasena/recupera-contrasena.module').then( m => m.RecuperaContrasenaPageModule)
+  },
 ];
 
 @NgModule({
--- src/app/login/login.page.ts
+++ src/app/login/login.page.ts
@@ -1,15 +1,74 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationExtras } from '@angular/router';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-login',
   templateUrl: './login.page.html',
   styleUrls: ['./login.page.scss'],
 })
+
+
 export class LoginPage implements OnInit {
+  users = [
+    { userName: 'Pato', password: 'pene' },
+    { userName: 'Ignacio', password: 'admi' },
+    { userName: 'user3', password: '1234' },
+  ];
+  user={
+    userName:"",
+    password:""
+  }
 
-  constructor() { }
+  constructor(
+    private router: Router,
+    private alertController: AlertController
+  ) { }
 
-  ngOnInit() {
+  ngOnInit() {}
+
+
+  async ingresar() {
+    const foundUser = this.users.find(
+      (u) => u.userName === this.user.userName && u.password === this.user.password
+    );
+
+    if (foundUser) {
+      let navigationExtras: NavigationExtras = {
+        state: {
+          user: this.user, // Passing the authenticated user
+        },
+      };
+      this.router.navigate(['/home'], navigationExtras);
+    } else if (this.user.userName === '' || this.user.password === '') {
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Asegúrate de ingresar datos en los campos',
+        buttons: ['Aceptar'],
+      });
+      await alert.present();
+    
+    
+    } else {
+      console.log('Credenciales incorrectas');
+      const alert = await this.alertController.create({
+        header: 'Error',
+        message: 'Usuario o contraseña incorrecto',
+        buttons: ['Aceptar'],
+      });
+      await alert.present();
+    }
+  }
+
+  recuperaContrasena() {
+    let navigationExtras: NavigationExtras ={
+      state: {
+        users: this.users // Pasar el arreglo de usuarios a la página de recupera-contrasena
+      }
+    };
+    this.router.navigate(['/recupera-contrasena'], navigationExtras);
   }
 
 }
+
+//recupera-contrasena
